06db2cf6ee7b5ce10c5d996f8523363e79ff7d22
Kodline123 <82050198+Kodline123@users.noreply.github.com>
2021-04-09 08:57:48 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,24 @@
-#Explain your work
+#Kullanıcı hesabı ödevi.
 
-#Question 1
-for x in range(a):
-	print(a)
+GİRİLENKULLANICIADI = input('KULLANICI ADINIZI GİRİNİZ:')
+GİRİLENŞİFRE = input('ŞİFRENİZİ GİRİNİZ:')
+print("KULLANICI HESABI OLUŞTURULDU BANKAMIZI KULLANDIĞINİZ İÇİN TEŞEKKÜRLER")
+kullanıcıseçimi = input('PARA GÖNDERME İŞLEMİ YAPMAK İSTİYORSANIZ EVET İSTEMİYORSANIZ HAYIR YAZINIZ:')
+if kullanıcıseçimi == "Evet" :
+
+
+    KULANİCİADİ = input('KULLANICI ADI GİRİNİZ:')
+    ŞİFRE = input('ŞİFRE GİRİNİZ:') 
+
+    if KULANİCİADİ == GİRİLENKULLANICIADI and ŞİFRE == GİRİLENŞİFRE :
+        İSİM = input('GÖNDERİLEN KİŞİ ADI')
+        SOYİSİM = input('SOYİSİM:')
+        YAŞ = int(input('YAŞINIZI GİRİNİZ:'))
+        TC = int(input('TC KİMLİK NO GİRİNİZ:'))
+        TUTAR = int(input('GÖNDERİLECEK TUTAR:'))
+        print(İSİM + " " + SOYİSİM + " " + str(YAŞ) + " " + str(TC) + " " + str(TUTAR)  + " " + 'TEŞEKKÜRLER İŞLEMİNİZ TAMAMLANDI')
+    else :
+        print("Kullanıcı adı ve şifre hatalı")
+else :
+    print("BANKAMIZI TERCİH ETTİĞİNİZ İÇİN TEŞEKKÜRLER BİR DAHA BEKLERİZ (: (:")
+
